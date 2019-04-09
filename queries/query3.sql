e8b7f1d7ab4fd7e7ef9b7943f247228ad909e08d
igormolchan <igor.molchan1992@gmail.com>
2019-04-09 15:53:17 +0300

    query 3 realisation

---
+++ queries/query3.sql
@@ -0,0 +1,15 @@
+﻿--QUERY-03 Вывод каталога с отступами в соответствии с уровнями каталога. Для каждого узла — число нижележащих узлов.
+   
+   SELECT LPAD(' ',5*(LEVEL-1))||code||': '||name "Code:Name"
+   FROM t_ctl_node
+   CONNECT BY PRIOR id_ctl_node=id_parent
+   START WITH CODE='all'
+   ORDER SIBLINGS BY code;
+   
+   SELECT ( COUNT(*) -1 ) number_of_childs, root
+   FROM ( SELECT connect_by_root(code) root
+   FROM t_ctl_node
+   CONNECT BY id_parent = PRIOR id_ctl_node)
+   GROUP BY root;
+
+   
