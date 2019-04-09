78de6d5b1c2cd64a0409efe7cb70b25fbe4af6ee
igormolchan <igor.molchan1992@gmail.com>
2019-04-09 15:45:48 +0300

    part of db population and query 1,4 realization

---
+++ queries/query1.sql
@@ -0,0 +1,9 @@
+﻿--QUERY-01 Найти товары, цены на которые отличаются от цены на модель.
+    
+    SELECT t_ware.id_ware, moniker, name, t_price_ware.price ware_price, t_price_model.price model_price
+    from t_ware 
+    INNER JOIN t_price_model
+    ON t_ware.id_model = t_price_model.id_model
+    INNER JOIN t_price_ware
+    ON t_ware.id_ware = t_price_ware.id_ware
+    WHERE t_price_ware.price!=t_price_model.price;
---
+++ queries/query4.sql
@@ -0,0 +1,7 @@
+﻿--QUERY-04 Вывод таблицы подразделений с отступами в соответствии с уровнями каталога
+   
+   SELECT LPAD(' ',5*(LEVEL-1))||name "Name"
+   FROM t_dept
+   CONNECT BY PRIOR id_dept=id_parent
+   START WITH UPPER(name)=UPPER('company')
+   ORDER SIBLINGS BY name;
